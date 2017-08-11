eb2cd1d56fef8ae3f19870245c5c5be87a4e8fc3
Vinayak <vinayakkini101@gmail.com>
2017-08-11 23:17:39 +0530

    realRank[] added

--- src/GFG.java
+++ src/GFG.java
@@ -7,11 +7,12 @@ class GFG {
 		Scanner sc= new Scanner(System.in);
 		System.out.println("Enter the text and the keyword: ");
         String text= sc.nextLine();
-        String keyword=sc.nextLine().toUpperCase();
+        String keyword=sc.nextLine();
         int t= text.length();
         int k= keyword.length();
         char[][] arr = new char[k][(t/k)+1];
 
+//        Store the ASCII values of chars in key[]
         int[] key = new int[k];
         for(int i=0; i<k; i++)
             key[i] = (int)keyword.charAt(i) ; 
@@ -19,6 +20,8 @@ class GFG {
         // for(int i=0; i<k; i++)
         //     System.out.print(key[i]+" ");
         
+        
+        //Store the text in a char[][]
         int count=0;
         for(int m=0; m<k; m++)
         {
@@ -28,19 +31,23 @@ class GFG {
         		{
         			arr[m][d] = (text.charAt(count)==' ') ? '_' :  text.charAt(count);
         			count++;
-        			System.out.print(arr[m][d]+" ");
+//        			System.out.print(arr[m][d]+" ");
         		}
         		else
         		{
         			arr[m][d] = '_';
         			count++;
-        			System.out.print(arr[m][d]+" ");
+//        			System.out.print(arr[m][d]+" ");
         		}
         	}
-        	System.out.println();
+//        	System.out.println();
         }
         
+        
+        //rank[] stores the rank of each char
+        //realRank[] gives the order of each column to be printed  i.e realRank[] stores index values of rank[]
         int[] rank= new int[k];
+        int[] realRank= new int[k];
         int ran=0;
         for(int i=0; i<k; i++)
         {
@@ -49,17 +56,22 @@ class GFG {
         	{
         		rank[i] = key[i]<key[j] ? rank[i] : ran++;
         	}
-//        	System.out.print(rank[i]+" ");
+        	realRank[rank[i]] = i;
         }
         
+//        for(int j=0; j<k; j++)
+//    	System.out.print(realRank[j]+" ");
         
+        
+        System.out.print("CT: ");
         for(int m=0; m<k; m++)
         {
         	for(int d=0; d<(t/k)+1; d++)
         	{
-        		System.out.print(arr[d][rank[m]]);
+        		System.out.print(arr[d][realRank[m]]);
         	}
         }
+        
         
         
 	}
